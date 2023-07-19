8f23c59df0abfa84f0e1bfa9389f18e80b20a616
Denise <deniselanzieri@gmail.com>
2023-07-19 19:26:06 +0200

    add new script

--- script/train_compressor_gnll.py
+++ script/train_compressor_gnll.py
@@ -1,14 +1,9 @@
-
-from functools import partial
 from tqdm import tqdm
 import pickle
-from pathlib import Path
 import tensorboard
-import numpy as np
 import jax
 import jax.numpy as jnp
 
-import flax.linen as nn
 from flax.metrics import tensorboard
 
 from haiku._src.nets.resnet import ResNet18
@@ -26,6 +21,7 @@ tfd = tfp.distributions
 from sbi_lens.gen_dataset.lensing_lognormal_dataset import LensingLogNormalDataset
 from sbi_lens.gen_dataset.utils import augmentation_noise, augmentation_flip
 from sbi_lens.config import config_lsst_y_10
+import pickle
 
 
 'unset XLA_FLAGS'
@@ -55,8 +51,9 @@ logger.addFilter(CheckTypesFilter())
 
 tfp.distributions.TransformedDistribution(tfp.distributions.Normal(0.0, 1.0), tfp.bijectors.Identity())
 
-
-
+####################################################################
+# Define configuration
+####################################################################
 N = config_lsst_y_10.N
 map_size = config_lsst_y_10.map_size
 sigma_e = config_lsst_y_10.sigma_e
@@ -82,40 +79,53 @@ augmentation = lambda example: augmentation_flip(
                        a=a,
                        b=b,
                        z0=z0))
-
-
+####################################################################
+# Build compressor
+####################################################################
 dim = 6
 aug_dim = int((dim * (dim + 1) / 2)+dim)
 
-# def aug_compressor(y):
-#     net_0=ResNet18(dim)(y,is_training=True)
-#     net=jax.nn.leaky_relu(net_0)
-#     net=hk.Linear(128)(net)
-#     net=jnp.tanh(net)
-#     net=hk.Linear(aug_dim)(net)
-#     return net  
+compressor = hk.transform_with_state(
+    lambda y : ResNet18(dim)(y, is_training=True)
+  )
 
-
-def aug_compressor(y):
-    net_0=ResNet18(128)(y,is_training=True)
-    net=jax.nn.leaky_relu(net_0)
-    net=hk.Linear(128)(net)
-    net=jnp.tanh(net)
-    net=hk.Linear(aug_dim)(net)
-    return net
-
-compressor =hk.transform_with_state(aug_compressor)
-
-
-parameters_compressor, opt_state_resnet = compressor.init(
+parameters_resnet, opt_state_resnet = compressor.init(
     jax.random.PRNGKey(0), y=0.5 * jnp.ones([1, N, N, nbins]))
 
+####################################################################
+# Create Density Estimetor for the compressor
+####################################################################
+class ConditionalMultivariateGaussian(hk.Module):
+
+  def __call__(self, x, dim=6):
+    net = jax.nn.leaky_relu(hk.Linear(256)(x))
+    net = jax.nn.tanh(hk.Linear(128)(net))
+    gaussian_mu = hk.Linear(dim)(net)
+    gaussian_tril = hk.Linear(dim * (dim + 1) // 2)(net)
+    
+    dist = tfd.MultivariateNormalTriL(loc=gaussian_mu, 
+                  scale_tril=tfp.bijectors.FillScaleTriL(
+                              diag_bijector=tfp.bijectors.Softplus(low=1e-3)
+                            )(gaussian_tril))                                  
+    
+    return dist
+
+model= hk.without_apply_rng(hk.transform(lambda theta, x : ConditionalMultivariateGaussian()(x).log_prob(theta).squeeze()))
+params_mdn =model.init(jax.random.PRNGKey(0),  theta=0.5*jnp.ones([1,6]), x=0.5*jnp.ones([1, 6]))
+####################################################################
+# Marge parameters
+####################################################################
+parameters_compressor= hk.data_structures.merge(
+    parameters_resnet,
+    params_mdn
+  )
+
+####################################################################
+# Dataset
+####################################################################
 ds = tfds.load('LensingLogNormalDataset/year_10_without_noise_score_density', 
                split='train[:80000]', 
                data_dir = 'tensorflow_dataset')
-
-
-
 
 ds = ds.repeat()
 ds = ds.shuffle(1000)
@@ -124,40 +134,41 @@ ds = ds.batch(128)
 ds = ds.prefetch(tf.data.experimental.AUTOTUNE)
 ds_train = iter(tfds.as_numpy(ds))
 
-
-
-total_steps=20_000
+####################################################################
+# Define configuration for the training
+####################################################################
+total_steps=150_000
 lr_scheduler = optax.piecewise_constant_schedule(
-    init_value=0.001,
+    init_value=0.0001,
     boundaries_and_scales={
-        int(5000): 0.1,
-        int(10000): 0.1,
-        int(15000): 0.1,
-        int(20000): 0.7,
-        int(30000): 0.7,
+        int(20000): 0.1,
+        int(40000): 0.1,
+        int(60000): 0.1,
+        int(80000): 0.7,
+        int(100000): 0.7,
+        int(120000): 0.7,
     })
 
-optimizer_c = optax.adam(learning_rate=lr_scheduler)
-opt_state_c = optimizer_c.init(parameters_compressor)
+optimizer = optax.chain(
+  optax.clip(1.0),
+  optax.adam(learning_rate=lr_scheduler)
+)
 
-
-
-
+opt_state_c = optimizer.init(parameters_compressor)
+####################################################################
+# Define loss and update function 
+####################################################################
 def loss_gnll(params, theta, x, state_resnet):
     y, opt_state_resnet = compressor.apply(params, state_resnet, None, x)
-    gmu = y[..., :6]
-    gtril = y[..., 6:]
-    dist = tfd.MultivariateNormalTriL(
-        loc=gmu,
-        scale_tril=tfp.bijectors.FillScaleTriL(
-            diag_bijector=tfp.bijectors.Softplus(),diag_shift=1e-03)(gtril)
-            )
-    return -jnp.mean(dist.log_prob(theta)), opt_state_resnet
+    log_prob =  model.apply(
+          params, 
+          theta, 
+          y)
+    return -jnp.mean(log_prob), opt_state_resnet
 
-
-
+@jax.jit    
 def update(
-    model_params,
+    params,
     opt_state,
     theta,
     x,
@@ -167,51 +178,48 @@ def update(
     (loss, opt_state_resnet), grads = jax.value_and_grad(
       loss_gnll,
       has_aux=True
-    )(model_params, theta, x, state_resnet)
+    )(params, theta, x, state_resnet)
 
-    updates, new_opt_state = optimizer_c.update(
+    updates, new_opt_state = optimizer.update(
       grads,
       opt_state
     )
 
     new_params = optax.apply_updates(
-      model_params,
+      params,
       updates
     )
 
     return loss, new_params, new_opt_state, opt_state_resnet
-
-
+####################################################################
+# TRAINING
+####################################################################
 store_loss = []
-
-
 summary_writer = tensorboard.SummaryWriter('logs/')
 
 for batch in tqdm(range(total_steps + 1)):
   ex = next(ds_train)
   if not jnp.isnan(ex['simulation']).any():
         l, parameters_compressor, opt_state_c, opt_state_resnet = update(
-            model_params=parameters_compressor,
+            params=parameters_compressor,
             opt_state=opt_state_c,
             theta=ex['theta'],
             x=ex['simulation'],
             state_resnet=opt_state_resnet)
-        summary_writer.scalar('train_loss_net27_tscript_nobott_res', l, batch)
-        summary_writer.scalar('learning_rate_net27_tscript_nobott_res',lr_scheduler(batch),  batch)
+        summary_writer.scalar('train_loss_var_gnll_smalllearning_150iter', l, batch)
+        summary_writer.scalar('learning_rate_var_gnll_smalllearning_150iter',lr_scheduler(batch),  batch)
         store_loss.append(l)
         if jnp.isnan(l):
           print('NaN Loss')
           break
-            
-
-
-import pickle
-
-with open("/gpfsdswork/projects/rech/ykz/ulm75uc/VMIM-vs-MSE-/data/params_compressor/net_27/params_nd_compressor_gnll_net27_tscript_nobott_res.pkl", "wb") as fp:
+####################################################################
+# SAVE FILE
+####################################################################
+with open("/gpfsdswork/projects/rech/ykz/ulm75uc/VMIM-vs-MSE-/data/params_compressor/params_nd_compressor_gnll.pkl", "wb") as fp:
   pickle.dump(parameters_compressor, fp)
 
-with open("/gpfsdswork/projects/rech/ykz/ulm75uc/VMIM-vs-MSE-/data/params_compressor/net_27/opt_state_resnet_gnll_net27_tscript_nobott_res.pkl", "wb") as fp:
+with open("/gpfsdswork/projects/rech/ykz/ulm75uc/VMIM-vs-MSE-/data/params_compressor/opt_state_resnet_gnll.pkl", "wb") as fp:
   pickle.dump(opt_state_resnet, fp)
 
-with open("/gpfsdswork/projects/rech/ykz/ulm75uc/VMIM-vs-MSE-/data/params_compressor/net_27/loss_compressor_gnll_net27_tscript_nobott_res.pkl", "wb") as fp:
+with open("/gpfsdswork/projects/rech/ykz/ulm75uc/VMIM-vs-MSE-/data/params_compressor/loss_compressor_gnll.pkl", "wb") as fp:
   pickle.dump(jnp.asarray(store_loss), fp)
