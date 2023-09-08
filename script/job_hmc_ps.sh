62ec3dbca7803d414b733d15b40b7b83383e2a0f
Denise <deniselanzieri@gmail.com>
2023-09-08 10:55:36 +0200

    rearrangement script folder

--- script/job_hmc_ps.sh
+++ script/job_hmc_ps.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 #SBATCH -A ykz@v100
-#SBATCH --job-name=name_of_the_job  
+#SBATCH --job-name=name_of_the_job         
 #SBATCH --output=name_of_the_job%j.out     
 #SBATCH --error=name_of_the_job%j.out     
-#SBATCH --array=21-31
 #SBATCH --constraint v100-32g
-#SBATCH --qos=qos_gpu-t3
+#SBATCH --qos=qos_gpu-t4
 #SBATCH --hint=nomultithread        
-#SBATCH -t 20:00:00
+#SBATCH -t 30:00:00
 #SBATCH --nodes=1
 #SBATCH --gpus-per-task=1
 #SBATCH --ntasks-per-node=1
@@ -17,4 +16,4 @@
 module purge
 module load tensorflow-gpu/py3/2.7.0
 
-python hmc_ff.py  --seed=$SLURM_ARRAY_TASK_ID --filename=job_$SLURM_ARRAY_TASK_ID
+python hmc_ps.py  
